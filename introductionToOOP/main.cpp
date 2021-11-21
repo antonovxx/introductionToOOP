d1016458d3061448a65dd19e186c21569c3aadaf
antonkurin <antonkurin1@gmail.com>
2021-11-21 19:55:34 +0300

    Initial Commit

---
+++ introductionToOOP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  introductionToOOP
+//
+//  Created by Антон Курин on 21.11.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
